a6289a8d501a0318439232ac94aa529111ab906f
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2023-02-09 15:01:15 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
+
+</script>
+
+<template>
+    <div class="home">
+      <h1>PokeDash</h1>
+      <h2>This is my PokeDash app</h2>
+      <HomeView />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.home{
+  background-color: $red;
+  h1{
+    color: $blue;
+  }
+  h2{
+    color: $yellow;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>This is my PokeDash!</h1>
+  </main>
+</template>
