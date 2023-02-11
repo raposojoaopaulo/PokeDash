8f3d1274cb242cbc0deedaad31e69d87e31b1b95
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2023-02-10 21:49:34 -0300

    backup de segurança

---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<template>
+  <main class="home">
+    <div class="home-logo">
+      <img src="/logo/Pokedash.png" alt="pokedash" />
+    </div>
+    <Search />
+  </main>
+</template>
+
+<style src="./style.scss" lang="scss" scoped />
