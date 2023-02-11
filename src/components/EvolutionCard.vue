9765236ec29c4479ee390d14f888aa1155e9e08b
Joao Paulo Raposo <raposojoaopaulo@gmail.com>
2023-02-11 12:32:26 -0300

    last adjusts

--- src/components/EvolutionCard.vue
+++ src/components/EvolutionCard.vue
@@ -1,8 +1,7 @@
 <script>
 
-import { usePokemonStore } from '@/stores/pokemon';
-
-const { getEvolutionChainNames } = usePokemonStore();
+// import { usePokemonStore } from '@/stores/pokemon';
+// const { evolutionChain, getEvolutionChainNames } = usePokemonStore();
 
 const evolutions = [];
 
@@ -14,33 +13,9 @@ const evolutions = [];
 
     methods: {      
 
-      evolutions: getEvolutionChainNames(evolutionChain),
+      // evolutions: getEvolutionChainNames(evolutionChain),
 
     },
-
-//     methods: {
-
-//       function getEvolutionChainNames(chain) {
-//   console.log("entrei na função")
-//       let species = [];
-//       species = [chain.species.name];
-//       console.log(`peguei o nome fora do if ${chain.species.name}`)
-//       console.log(`specie: ${species}`)
-      
-//       for (let i = 0; i < chain.evolves_to.length; i++) {
-//         species = species.concat(getSpeciesNames(chain.evolves_to[i]));
-//         console.log(`peguei o name no for: ${species}`)
-//       }
-//       console.log(`specie final: ${typeof species}`)
-//       return species;
-// };
-
-// evolutions = getEvolutionChainNames(evolutionChain);
-
-
-//     },
-
-
   }
 </script>
 
